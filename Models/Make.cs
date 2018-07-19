ec0c13ac346d8024dfec02c5f54598865a067da9
George Codat Tudoran <office@solcreation.ro>
2018-07-19 21:44:24 +0300

    Fix small bug

--- Models/Make.cs
+++ Models/Make.cs
@@ -5,7 +5,7 @@ namespace vega.Models
 {
     public class Make
     {
-        public int Id {get; set}
+        public int Id {get; set; }
         public string Name {get; set;}
 
         public ICollection<Model> Models {get; set; }
