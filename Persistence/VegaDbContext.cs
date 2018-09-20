f8e9ee43ff8638d91da231a31128fb200a0f86c1
George Codat Tudoran <office@solcreation.ro>
2018-09-20 11:05:47 +0300

    Creating a database

--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using vega.Models;
 
 namespace vega.Persistence
 {
@@ -7,7 +8,8 @@ namespace vega.Persistence
         public VegaDbContext(DbContextOptions<VegaDbContext> options)
             : base(options)
             {
-
             }
+
+             public DbSet<Make> Makes {get; set; }
     }
 }
