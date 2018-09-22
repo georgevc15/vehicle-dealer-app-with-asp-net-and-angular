c56b9db3b6f49999f296d6fb0ae30c001bcfd735
George Codat Tudoran <office@solcreation.ro>
2018-09-22 17:28:27 +0300

    Adding resources

--- Models/Make.cs
+++ Models/Make.cs
@@ -6,17 +6,6 @@ namespace vega.Models
 {
     public class Make
     {
-        public int Id {get; set; }
 
-        [Required]
-        [StringLength(255)]
-        public string Name {get; set;}
-
-        public ICollection<Model> Models {get; set; }
-
-        public  Make()
-        {
-            Models = new Collection<Model>();
-        }
     }
 }
--- Models/Model.cs
+++ Models/Model.cs
@@ -6,15 +6,7 @@ namespace vega.Models
     [Table("Models")]
     public class Model
     {
-        public int Id {get; set; }
-               
-        [Required]
-        [StringLength(255)]
-        public string Name {get; set; }
 
-        public Make Make { get; set; }
-
-        public int MakeId {get; set; }
 
     }
 }
