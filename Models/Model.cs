b9ca6879e0b1a9e6bfc5157fde5e89192732ef36
George Codat Tudoran <office@solcreation.ro>
2018-09-23 14:29:35 +0300

    Mapping models and makes, model for features

--- Models/Model.cs
+++ Models/Model.cs
@@ -6,7 +6,15 @@ namespace vega.Models
     [Table("Models")]
     public class Model
     {
+        public int Id {get; set; }
+        
+        [Required]
+        [StringLength(255)]
+        public string Name {get; set; }
 
+        public Make Make { get; set; }
+
+        public int MakeId {get; set; }
 
     }
 }
--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -11,6 +11,6 @@ namespace vega.Persistence
             }
 
         public DbSet<Make> Makes {get; set;}
-
+        public DbSet<Features> Features {get; set;}
     }
 }
