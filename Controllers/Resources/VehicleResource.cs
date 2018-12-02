c1b7988f54184c2567c502a145db22d5f31d4028
George Codat Tudoran <office@solcreation.ro>
2018-12-02 11:59:54 +0200

    Implementing Server-side Validation

--- Controllers/Resources/VehicleResource.cs
+++ Controllers/Resources/VehicleResource.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 using vega.Models;
 
 namespace vega.Controllers.Resources
@@ -12,6 +13,7 @@ namespace vega.Controllers.Resources
         public int ModelId { get; set; }
         public bool isRegistered { get; set; }
 
+        [Required]
         public ContactResource Contact {get; set; }
         
         public ICollection<int> Features { get; set; }
--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -8,6 +8,7 @@ namespace vega.Persistence
         public DbSet<Vehicle> Vehicles { get; set; }
         public DbSet<Make> Makes {get; set;}
         public DbSet<Features> Features {get; set;}
+        public DbSet<Model> Models {get; set; }
 
         public VegaDbContext(DbContextOptions<VegaDbContext> options)
             : base(options)
