9b7f8f67e5560933c39a59a60a4f85ef378ce318
George Codat Tudoran <office@solcreation.ro>
2018-12-18 19:01:54 +0200

    Get one vehicle

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -74,5 +74,18 @@ namespace vega.Controllers
 
             return Ok(id);
         }
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetVehicle(int id) {
+
+            var vehicle = await context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.id == id);
+            if(vehicle == null)
+                return NotFound();
+
+            var vehicleResource = mapper.Map<Vehicle, VehicleResource>(vehicle);
+
+            return Ok(vehicleResource);    
+        }
     }
 }
