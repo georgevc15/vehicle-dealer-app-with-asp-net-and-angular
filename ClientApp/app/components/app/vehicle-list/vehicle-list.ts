346c27d90ef388fe2cf8dd3f31f30c60e49864f7
George Codat Tudoran <office@solcreation.ro>
2019-03-19 18:40:15 +0200

    Begin work on filter module

--- ClientApp/app/components/app/vehicle-list/vehicle-list.ts
+++ ClientApp/app/components/app/vehicle-list/vehicle-list.ts
@@ -1,8 +1,8 @@
-
 import { Component, OnInit } from '@angular/core';
 import { Vehicle } from '../models/vehicle';
 import { VehicleService } from './../../../services/vehicle.service';
 import { Observable } from 'rxjs/Observable';
+import { KeyValuePair } from './../models/vehicle';
 
 @Component({
     templateUrl: 'vehicle-list.html'
@@ -10,12 +10,22 @@ import { Observable } from 'rxjs/Observable';
 
 export class VehicleListComponent implements OnInit {
     vehicles: any = {};
+    makes: KeyValuePair[] = [];
+    filter: any = {};
 
     constructor(private VehicleService: VehicleService) {}
 
     ngOnInit() {
+        this.VehicleService.getMakes()
+            .subscribe(makes => this.makes = makes);
+
         this.VehicleService.getVehicles()
             .subscribe(vehicles => this.vehicles = 'any');
+    }
+
+    onFilterChnage() {
+        //this.filter.makeId
+        console.log('Filter');
     }
 
 }
